258330dd9f5893d8a0a6b9b8c791ac612fdb1c22
Sebastian <seba.krawczyk94@gmail.com>
2022-05-30 15:26:56 +0200

    PostgreSQL

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://Sebastian:kupajeza@localhost/TodoApplicationDatabase"
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgres1l://", 1)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
